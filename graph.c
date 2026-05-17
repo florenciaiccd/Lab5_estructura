73b8bb2c49c02c32f7b275df195c582878117227
Your Name <you@example.com>
2026-05-17 23:07:42 +0000

    new try

--- graph.c
+++ graph.c
@@ -60,7 +60,7 @@ void addEdge(Graph* g, const char* src, const char* dest, int weight) {
     nuevo_Edge -> weight = weight;
     nuevo_Edge -> target = copia_dest;
 
-    list_pushBack( g -> adjacencyMap, nuevo_Edge);
+    list_pushBack( src -> Edge, nuevo_Edge);
     
 
 }
