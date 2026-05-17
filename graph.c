de8029ca6b93640f4d1d38c9cc3a94394040f01e
Your Name <you@example.com>
2026-05-17 23:18:06 +0000

    new try

--- graph.c
+++ graph.c
@@ -60,7 +60,7 @@ void addEdge(Graph* g, const char* src, const char* dest, int weight) {
     nuevo_Edge -> weight = weight;
     nuevo_Edge -> target = copia_dest;
 
-    List * lista_adyacencia = map_search(g -> adjacencyMap, src);
+    MapPair * lista_adyacencia = map_search(g -> adjacencyMap, src);
 
     list_pushBack(lista_adyacencia,nuevo_Edge);
     
