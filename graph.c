bb5720a8214164ee8d1c9b96fb61ce2f0fd214b4
Your Name <you@example.com>
2026-05-18 00:08:16 +0000

    new try

--- graph.c
+++ graph.c
@@ -104,7 +104,7 @@ int getWeight(Graph* g, const char* label1, const char* label2) {
 List* getAdjacentLabels(Graph* g, const char* label) {
     if (!g || !label) return NULL;
 
-    MapPair * pair = map_search(g -> adjacencyMap, (void*)label1);
+    MapPair * pair = map_search(g -> adjacencyMap, (void*)label);
 
     if(pair != NULL){
         List * lista_aristas = (List*)pair -> value;
